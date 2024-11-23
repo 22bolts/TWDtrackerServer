3c47f9616f2e4fb410306a581e23680d2b4bc08d
22bolts <bguy00504@gmail.com>
2024-11-23 09:59:43 +0100

    Users mutation removed

--- src/Schema/index.ts
+++ src/Schema/index.ts
@@ -1,22 +1,22 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import { CREATE_USER, SIGN_IN } from "./Mutations/Users";
-import { GET_ALL_USERS, GET_USER_BY_ID } from "./Queries/User";
+// import { CREATE_USER, SIGN_IN } from "./Mutations/Users";
+// import { GET_ALL_USERS, GET_USER_BY_ID } from "./Queries/User";
 // import { CREATE_TRANSACTION } from "./Mutations/Transactions";
 
 const RootQuery = new GraphQLObjectType({
     name: "query",
     fields: {
-        getAllUsers: GET_ALL_USERS,
-        getUserById: GET_USER_BY_ID,
+        // getAllUsers: GET_ALL_USERS,
+        // getUserById: GET_USER_BY_ID,
 
-        signIn: SIGN_IN,
+        // signIn: SIGN_IN,
     }
 })
 
 const Mutation = new GraphQLObjectType({
     name: "mutation",
     fields: {
-        createUser: CREATE_USER,
+        // createUser: CREATE_USER,
         // updatePassword: UPDATE_PASSWORD,
         // deleteUser: DELETE_USER,
 
